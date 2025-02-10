f40d06c3487a1398592b0b32adb01526cede9e11
Narimasa Sakurai <whiteonyx@gmail.com>
2025-02-11 08:55:15 +0900

    不要なコードを削除 Fixes #5

--- public/background.js
+++ public/background.js
@@ -1,14 +1,3 @@
 chrome.sidePanel
   .setPanelBehavior({ openPanelOnActionClick: true })
   .catch((error) => console.error(error));
-
-chrome.action.onClicked.addListener((tab) => {
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: getHtml
-  });
-});
-function getHtml() {
-  const html = document.documentElement.outerHTML;
-  console.log(html); // You can also send this to the background script or store it as needed
-}
